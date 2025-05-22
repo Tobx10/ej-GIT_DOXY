19cf11ae40ca0709960888d4e1f997ae7f7c3ab4
Tobias Gonzalez <tobygonzalezb@gmail.com>
2025-05-21 21:15:35 -0300

    Hola pepe este es un ejemplito :p

---
+++ ejemploGit_Doxy.c
@@ -0,0 +1,20 @@
+/**
+* @struct Persona
+ * @brief Representa una persona con nombre y edad
+ */
+ typedef struct {
+ char nombre[50]; /**< Nombre de la persona */
+ int edad; /**< Edad de la persona */
+ } Persona;
+
+/**
+* @brief Suma dos numeros enteros
+ *
+ * @param a Primer operando
+ * @param b Segundo operando
+ * @return int Resultado de la suma
+ **/
+
+int sumar(int a, int b) {
+ return a + b;
+ }
